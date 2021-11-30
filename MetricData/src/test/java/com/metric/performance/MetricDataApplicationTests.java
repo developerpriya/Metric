d0326effa96177df87f07054bb143b0a491f0707
developerpriya <45784795+developerpriya@users.noreply.github.com>
2021-11-30 09:38:40 +0400

    Add files via upload

---
+++ MetricData/src/test/java/com/metric/performance/MetricDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metric.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
